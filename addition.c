cbb13013cbf06c226c120ff00b1fe8e02eaaf41b
Chetan Chavan <114980932+Chetan123chavan@users.noreply.github.com>
2025-02-11 09:09:37 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,32 @@
+/// Addition of two numbers using third variable
+
+#include<stdio.h>
+int main()
+{
+  int x=10, y=20,z;
+  z=x+y;
+  printf("Sum=%d",z);
+}
+
+/// Addition of two numbers without using third variable
+
+#include<stdio.h>
+int main()
+{
+  int x=10,y=20;
+  printf("Sum=%d",x+y);
+}
+
+/// Addition of two numbers by taking values from user
+
+#include<stdio.h>
+{
+  int x,y;
+  printf("Enter first number");
+  scanf("%d",&x);
+  printf("Enter second number");
+  scanf("%d",&y);
+  printf("Sum=%d",x+y);
+  // display in the format like 10 + 20 = 30
+  printf("%d + %d = %d",x,y,x+y);
+}
