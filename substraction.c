5444a90274d43293032b970f65936498d0e7b9ce
Chetan Chavan <114980932+Chetan123chavan@users.noreply.github.com>
2025-02-11 09:27:03 +0530

    Update substraction.c

--- substraction.c
+++ substraction.c
@@ -1,1 +1,32 @@
-ww
+/// Substraction of two numbers using third variable
+
+#include<stdio.h>
+int main()
+{
+  int x=10, y=20,z;
+  z=x-y;
+  printf("Sum=%d",z);
+}
+
+/// Substraction of two numbers without using third variable
+
+#include<stdio.h>
+int main()
+{
+  int x=10,y=20;
+  printf("Sum=%d",x-y);
+}
+
+/// Substraction of two numbers by taking values from user
+
+#include<stdio.h>
+{
+  int x,y;
+  printf("Enter first number");
+  scanf("%d",&x);
+  printf("Enter second number");
+  scanf("%d",&y);
+  printf("Difference=%d",x-y);
+  // display in the format like 20 - 10 = 10
+  printf("%d + %d = %d",x,y,x-y);
+}
